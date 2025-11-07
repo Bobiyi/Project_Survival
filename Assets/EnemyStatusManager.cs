46e0cbd0c5746bf57b309eb63ec54a79f441df10
eik123 <erikadolfiskola@gmail.com>
2025-11-07 11:30:18 +0100

    damage system kész

--- Assets/EnemyStatusManager.cs
+++ Assets/EnemyStatusManager.cs
@@ -7,8 +7,11 @@ public class EnemyStatusManager : MonoBehaviour
     [SerializeField] private float hp;
     [SerializeField] private float speed;
     [SerializeField] private GameObject XPpref;
+    [SerializeField] private SpriteRenderer sprite;
+
     //[SerializeField] private float hp;
 
+    private Color originalColor;
     private bool garlicHasFirstHit;
 
     public bool GarlicHasFirstHit { get => garlicHasFirstHit; set => garlicHasFirstHit = value; }
@@ -17,7 +20,9 @@ public class EnemyStatusManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
         GarlicHasFirstHit = false;
+        originalColor = sprite.color;
     }
 
     // Update is called once per frame
@@ -25,15 +30,38 @@ public class EnemyStatusManager : MonoBehaviour
     {
         
     }
+  
     public void Damaged(float damage)
     {
         hp -= damage; 
         if(hp <= 0)
         {
+            
             Instantiate(XPpref,
                 position: new Vector2(transform.position.x, transform.position.y - 0.75f),
                 rotation: Quaternion.identity);
             Destroy(gameObject);
         }
+        else
+        {
+            StartCoroutine(ColorChange());
+        }
     }
+
+    IEnumerator ColorChange()
+    {
+        sprite.color = new Color(
+            r: 255,
+            g: 0,
+            b: 0);
+
+        yield return new WaitForSeconds(0.5f);
+
+        new WaitForSeconds(0.5f);
+        sprite.color = new Color(
+            r: originalColor.r,
+            g: originalColor.g,
+            b: originalColor.b);
+    }
+
 }
